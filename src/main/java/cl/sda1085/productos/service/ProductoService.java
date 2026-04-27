16d96fdfa3291452bd2e38d3f6cc084f462bb2e2
Gonza-2103 <ghormazabal.d@gmail.com>
2026-04-27 15:43:13 -0400

    Clase 'productoResponseDTO' y 'productoService'

--- src/main/java/cl/sda1085/productos/service/ProductoService.java
+++ src/main/java/cl/sda1085/productos/service/ProductoService.java
@@ -1,8 +1,13 @@
 package cl.sda1085.productos.service;
 
+import cl.sda1085.productos.dto.ProductoResponseDTO;
+import cl.sda1085.productos.model.Producto;
 import cl.sda1085.productos.repository.ProductoRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -12,5 +17,33 @@ public class ProductoService {
     //Vínculo con 'repository'
     private final ProductoRepository productoRepository;
 
+    private ProductoResponseDTO mapToDTO(Producto producto){
+        return new ProductoResponseDTO(
+                producto.getId(),
+                producto.getNombre(),
+                producto.getDescripcion(),
+                producto.getAutor(),
+                producto.getPeriodo(),
+                producto.getTecnica(),
+                producto.getDimensiones(),
+                producto.getPrecioBase(),
+                producto.getEstadoAutenticidad(),
+                producto.getUrlImagen(),
+                producto.getIdCategoria(),
+                producto.getIdVendedor()
+        );
+    }
 
+    //Obtener todos
+    public List<ProductoResponseDTO> obtenerTodos(){
+        return productoRepository.findAll()
+                .stream()
+                .map(this::mapToDTO)
+                .collect(Collectors.toList());
+    }
+
+    //Obtener por ID
+    //Guardar
+    //Actualizar
+    //Eliminar
 }
